cd78d3324ba32e8afe053b2266ecb926bc15f1f9
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-26 11:56:33 -0800

    call express listen to port
    called express in the app variable and started listening on the defined port.

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
