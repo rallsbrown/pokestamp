4ac24e6610ab9232a1c2bd564cc974cad0a9da01
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-26 12:39:50 -0800

    added test API call route
    added backend route that makes API call using dotenv for key stored in .env

--- server/index.js
+++ server/index.js
@@ -10,4 +10,20 @@ app.get("/", (req, res) => {
   res.json("hi");
 });
 
+app.get("/pokemon", (req, res) => {
+  const options = {
+    method: "GET",
+    url: `${process.env.POKEAPI_KEY}pokemon?limit=151`,
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
