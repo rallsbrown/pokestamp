7fb5d6c3e7ac61aa4c30833ba05f31a782096afb
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-26 12:11:19 -0800

    test get route
    added a quick test route

--- server/index.js
+++ server/index.js
@@ -6,4 +6,8 @@ require("dotenv").config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("hi");
+});
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
