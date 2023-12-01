d44f0d3d0fe9a28b7db5a9feb231838b6e4f026b
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-30 16:53:09 -0800

    made test component
    made a test list component to pull API request from proxy server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import TestList from "./components/TestList/TestList";
 
 function App() {
   return (
-    <>
+    <div>
       <h1>p o k e s t a m p</h1>
-    </>
+      <TestList />
+      <div>
+        <h3>filler</h3>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/TestList/TestList.jsx
@@ -0,0 +1,9 @@
+const TestList = () => {
+  return (
+    <div>
+      <h2>t e s t l i s t.</h2>
+    </div>
+  );
+};
+
+export default TestList;
