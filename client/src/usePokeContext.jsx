28d5cfa027ce8e1158ab0e53bc363c689ed25a9f
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-30 18:40:55 -0800

    cleaned up bad naming
    changed useContext.jsx to usePokeContext.jsx to avoid confusion/issues

--- client/src/usePokeContext.jsx
+++ client/src/usePokeContext.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PokeContext } from "./PokeContext";
 
-export const useContext = () => {
+export const usePokeContext = () => {
   return React.useContext(PokeContext);
 };
