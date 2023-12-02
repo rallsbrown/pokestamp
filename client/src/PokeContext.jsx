9d43e79a9956c237981af63975521dc70ef3cab9
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-12-01 17:30:56 -0800

    Update PokeContext.jsx
    cleaned up PokeContext.jsx

--- client/src/PokeContext.jsx
+++ client/src/PokeContext.jsx
@@ -3,33 +3,28 @@ import axios from "axios";
 
 export const PokeContext = React.createContext(null);
 
-const Provider = (props) => {
+const Provider = ({ children }) => {
   const [pokeList, setPokeList] = useState([]);
   const [loading, setLoading] = useState(false);
 
   //got stuck on this
   //Uncaught Error: Objects are not valid as a React child (found: object with keys {name, url})
   //can't render an object to the DOM
-  const getPoke = React.useCallback(async () => {
+  const getPoke = async () => {
     setLoading(true);
     try {
       const { data } = await axios.get("http://localhost:8000/pokemon");
-      // const pokeUrls = data.map((poke) => poke.url);
-      // const pokeNames = data.map((poke) => poke.name);
-      // console.log("poke urls", pokeUrls);
-      // console.log("poke names", pokeNames);
       setPokeList(data);
-      console.log("fetched data", data);
+      // console.log("fetched data", data);
       setLoading(false);
     } catch (e) {
       console.log(e);
       setLoading(false);
     }
-  }, []);
+  };
 
   React.useEffect(() => {
     getPoke();
-    console.log("pokeList-useEffect", pokeList);
   }, []);
 
   const contextValue = {
@@ -39,9 +34,7 @@ const Provider = (props) => {
   };
 
   return (
-    <PokeContext.Provider value={contextValue}>
-      {props.children}
-    </PokeContext.Provider>
+    <PokeContext.Provider value={contextValue}>{children}</PokeContext.Provider>
   );
 };
 
