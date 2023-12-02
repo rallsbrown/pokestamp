cabea0afcde5bdc7b6ca9faceb4eebf3f245e62c
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-12-01 17:46:19 -0800

    created PokeList component
    created PokeList component jsx/css and imported into App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Provider from "./PokeContext";
 import TestList from "./components/TestList/TestList";
+import PokeList from "./components/TestList/PokeList/PokeList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>p o k e s t a m p</h1>
       <div>
         <Provider>
+          <PokeList />
           <TestList />
         </Provider>
       </div>
---
+++ client/src/components/TestList/PokeList/PokeList.jsx
@@ -0,0 +1,5 @@
+const PokeList = () => {
+  return <div>p o k e l i s t</div>;
+};
+
+export default PokeList;
