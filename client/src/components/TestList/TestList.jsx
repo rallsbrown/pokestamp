da4f7604882e814491e7ca9e93c51f843ba5bd4f
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-12-01 17:29:27 -0800

    Update TestList.jsx
    cleaned up testList.jsx

--- client/src/components/TestList/TestList.jsx
+++ client/src/components/TestList/TestList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import { usePokeContext } from "../../usePokeContext";
 
 const TestList = () => {
@@ -10,44 +9,17 @@ const TestList = () => {
   if (!loading && !pokeList.length) {
     return <div>no pokemon</div>;
   }
-  // const [results, setResults] = React.useState([]);
-
-  // const getList = () => {
-  //   const options = {
-  //     method: "GET",
-  //     url: "http://localhost:8000/pokemon",
-  //   };
-
-  //   axios
-  //     .request(options)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setResults(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-
-  // const handleGetList = () => {
-  //   console.log("getList");
-  //   const list = results;
-  //   getList();
-  //   console.log("the list", list);
-  // };
 
   return (
     <div>
       <h2>t e s t l i s t.</h2>
-      {pokeList.map((l, i) => {
+      {pokeList.map((poke, i) => {
         return (
           <div key={i}>
-            name: {l.name}, url: {l.url}
+            name: {poke.name}, url: {poke.url}
           </div>
         );
       })}
-      {/* <button onClick={handleGetList}>get list</button>
-      <p>{JSON.stringify(results)}</p> */}
     </div>
   );
 };
