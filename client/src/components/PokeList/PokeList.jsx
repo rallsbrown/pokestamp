db933d05504267f36d91cddb482d1f5f80260d13
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-12-01 18:33:25 -0800

    updated PokeList
    built out some temp structure/styling

--- client/src/components/PokeList/PokeList.jsx
+++ client/src/components/PokeList/PokeList.jsx
@@ -1,10 +1,28 @@
-import PokeCard from "../../PokeCard/PokeCard";
+import React, { useState } from "react";
+import "./PokeList.css";
+import { usePokeContext } from "../../usePokeContext";
+import PokeCard from "../PokeCard/PokeCard";
 
 const PokeList = () => {
+  //do i want a global isloading state, or per component?
+  // eslint-disable-next-line no-unused-vars
+  const { pokeList, setPokeList, loading } = usePokeContext();
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
   return (
     <>
-      <PokeCard />
-      <div className='PokeList'>p o k e l i s t</div>
+      <div className='PokeList'>
+        <h2> p o k e l i s t </h2>
+        <div className='ListButtons'>
+          <button className='ListButton'>get random pokemon</button>
+          <button className='ListButton'>clear pokemon</button>
+        </div>
+        <div className='PokeCards'>
+          <PokeCard />
+        </div>
+      </div>
     </>
   );
 };
